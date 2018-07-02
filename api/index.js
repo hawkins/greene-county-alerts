95f072ee1e9a83709c0ad2bc75859e9dac7039e9
Josh Hawkins <hawkins@users.noreply.github.com>
2018-07-02 13:50:28 -0400

    Set up multi county data
    Not yet fully served in api

--- api/index.js
+++ api/index.js
@@ -1,74 +1,86 @@
-const app = require("express")();
-const graphqlHTTP = require("express-graphql");
-const { buildSchema } = require("graphql");
-const request = require("request");
-const { parseString } = require("xml2js");
+const app = require('express')();
+const graphqlHTTP = require('express-graphql');
+const {buildSchema} = require('graphql');
+const request = require('request');
+const {parseString} = require('xml2js');
 
-const URL = "http://www.co.greene.oh.us/RSSFeed.aspx?ModID=63&CID=All-0";
+const URLS = {
+  green: 'http://www.co.greene.oh.us/RSSFeed.aspx?ModID=63&CID=All-0',
+  montgomery: 'https://www.daytonohio.gov/RSSFeed.aspx?ModID=63&CID=All-0',
+};
 
 /* Data */
 let data;
 const getAlerts = () => {
-  try {
-    request(URL, (err, res, body) => {
-      if (err) throw err;
+  // Reset data
+  data = {};
 
-      parseString(body, { explicitArray: false }, (parseErr, json) => {
-        if (parseErr) throw err;
+  // Aggregate county data
+  Object.keys(URLS).map(county => {
+    try {
+      request(URLS[county], (err, res, body) => {
+        if (err) throw err;
 
-        data = json.rss.channel;
-        data.date = data.lastBuildDate;
-        data.alerts = data.item ? data.item.slice() : [];
-        delete data.item;
+        parseString(body, {explicitArray: false}, (parserErr, json) => {
+          if (parserErr) throw err;
 
-        console.log(
-          "Updated data. There are currently " +
-            data.alerts.length +
-            " alerts in effect."
-        );
+          data[county] = json.rss.channel;
+          data[county].date = data[county].lastBuildDate;
+          data[county].alerts = data[county].item
+            ? data[county].item.slice()
+            : [];
+          delete data[county].item;
+
+          console.log(
+            `Updated data for ${county} county. There are currently ${
+              data[county].alerts.length
+            } alerts in effect.`,
+          );
+        });
       });
-    });
-  } catch (err) {
-    console.error("Failed to register new alert data:");
-    console.error(err);
-  }
+    } catch (err) {
+      console.error(`Failed to register new alert data for ${county} county:`);
+      console.error(err);
+    }
+  });
 };
 setInterval(getAlerts, 900000); // Every 15 mins
 getAlerts();
 
 /* GraphQL */
 const schema = buildSchema(`
-type Channel {
-  title: String
-  link: String
-  date: String
-  description: String
-  language: String
-  alerts: [Alert]
-}
+  type Channel{
+    title : String
+    link : String
+    date : String
+    description : String
+    language : String
+    alerts : [Alert]
+  }
 
-type Alert {
-  pubDate: String
-  title: String
-  link: String
-  description: String
-}
+  type Alert{
+    pubDate : String
+    title : String
+    link : String
+    description : String
+  }
 
-type Query {
-  alerts: [Alert]
-  channel: Channel
-}
+  type Query {
+    alerts: [Alert] channel : Channel
+  }
 `);
-const rootValue = { channel: () => data, alerts: () => data.alerts };
+const rootValue = {
+  channel: () => data['greene'], // TODO: Make this query dynamic
+  alerts: () => {
+    // TODO: Make this query also check for county and be specific if provided
+    return [].concat.apply(
+      [],
+      Object.keys(data).map(county => data[county].alerts),
+    );
+  },
+};
 
-app.use(
-  "/",
-  graphqlHTTP({
-    schema,
-    rootValue,
-    graphiql: true
-  })
-);
+app.use('/', graphqlHTTP({schema, rootValue, graphiql: true}));
 
-const port = process.env.NODE_ENV === "production" ? 80 : 3000;
-app.listen(port, () => console.log("Listening on port " + port));
+const port = process.env.NODE_ENV === 'production' ? 80 : 3000;
+app.listen(port, () => console.log('Listening on port ' + port));
